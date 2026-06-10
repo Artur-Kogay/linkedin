5f0b8dd9b8aefc88cc0523d5fadf66816e509b71
Artur-Kogay <9571artur9571@gmail.com>
2026-06-11 01:47:09 +0600

    feature-ANUI-2: Add: styles for hero image and fix margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
     <main className={styles.main__content}>
       <Container>
         <section className={styles.hero}>
-          <div>
-            <h1>Добро пожаловать <br /> в сообщество <br /> специалистов!</h1>
+          <div className={styles.hero__left}>
+            <h1 className={styles.title}>Добро пожаловать <br /> в сообщество <br /> специалистов!</h1>
           </div>
-          <div>
-            <HeroImage />
+          <div className={styles.hero__right}>
+            <HeroImage className={styles.hero__right_image}/>
           </div>
         </section>
       </Container>
